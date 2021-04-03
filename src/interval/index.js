dc460eec2b9cd51306c83cbe7ecce9c38b6d3720
tobey.allison <tobey.allison@emit.technology>
2021-04-03 22:50:17 +0800

    remove unused js

--- src/interval/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Interval = /** @class */ (function () {
-    function Interval() {
-    }
-    return Interval;
-}());
-exports.default = Interval;
--- src/pages/epoch/miner/altar.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./index");
-var miner = new index_1.default(index_1.MinerScenes.altar);
-exports.default = miner;
