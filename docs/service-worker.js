f9587a765b493572790b303913f92c791564768a
tobey.allison <tobey.allison@emit.technology>
2023-01-05 14:33:27 +0800

    Added the feature of reset account by private key

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6606e318fdaef3b08f7ac8aa9f3095a8.js"
+  "./precache-manifest.bf558a03282a2b52796382b38e6302cf.js"
 );
 
 self.addEventListener('message', (event) => {
